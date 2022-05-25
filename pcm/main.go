adfc7c0032848200dad48080e0ba6fc428f24559
Kees van Voorthuizen <me@keesvv.nl>
2022-05-26 01:42:33 +0200

    feat(pcm): add initial semi-working concept
    It's late, I'm tired and I've been reading about DSP and sine wave
    sampling all day long. Glad it's somewhat working. The formula for
    `pos` is broken, will figure that out later.

---
+++ pcm/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/mjibson/go-dsp/wav"
+)
+
+func main() {
+	freq := flag.Float64("f", 1000, "frequency (Hz)")
+	flag.Parse()
+
+	w, _ := wav.New(os.Stdin)
+	nSamples := int(float64(w.SampleRate) / *freq)
+
+	for i := 0; ; i++ {
+		f, err := w.ReadFloats(nSamples)
+
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+
+		pos := time.Duration(
+			((float64(i) * *freq) / float64(w.SampleRate)) * float64(time.Millisecond),
+		)
+		fmt.Printf("cycle %d pos %v %v\n", i+1, pos, f)
+		// time.Sleep(time.Millisecond * 50)
+
+		// var samples []float64
+		// for _, s := range f {
+		// 	samples = append(samples, float64(s))
+		// 	fmt.Println(s)
+		// }
+
+		// window.Apply(samples, window.Hann)
+
+		// sfft := fft.FFTReal(samples)
+		// for _, f := range sfft {
+		// 	fmt.Println(cmplx.Abs(f))
+		// }
+	}
+}
